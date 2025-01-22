680b0e6c5edff95bca31c313f00576226d639c07
OXplakhoa <OXplakhoa@gmail.com>
2025-01-22 21:01:37 +0700

    Add Quiz & Shuffle Ans

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,3 +1,28 @@
-export default function Quiz(){
-    
+import { useState } from "react";
+import questions from "../questions";
+
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
+  const activeQuestionIdx = userAnswer.length;
+  const shuffleAns = [...questions[activeQuestionIdx].answers];
+  shuffleAns.sort(() => Math.random() - 0.5);
+  const handleAnswer = (selectedAnswer) => {
+    setUserAnswer((prev) => {
+      return [...prev, selectedAnswer];
+    });
+  };
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{questions[activeQuestionIdx].text}</h2>
+        <ul id="answers">
+          {shuffleAns.map((ans) => (
+            <li key={ans} className="answer">
+              <button onClick={() => handleAnswer(ans)}>{ans}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
